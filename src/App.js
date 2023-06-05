d88a3bf6b0a7b80ce20b8223e9cd78e98b6120ee
Ali Abdukadyrov <vip.astiali@gmail.com>
2023-06-05 13:44:20 +0600

    home21

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoList } from './component/firebase-todo/TodoList';
+function App() {
+  return (
+    <div className="App">
+     <h2>Hello FireBase</h2>
+     <TodoList/>
+    </div>
+  );
+}
+
+export default App;
